8f44ba4403d70aecde438f664d68b417c64abee8
Kate Kunnykova <kate@Kates-MacBook-Pro.local>
2024-06-22 10:16:52 -0400

    Added outputFolder for allure playwrght report in playwright.config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [ ['line'], ['allure-playwright'] ],
+  reporter: [ ['line'], ['allure-playwright', {outputFolder: 'tests-results'}] ],
 
   globalSetup: require.resolve('./utils/globalSetup'),
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
